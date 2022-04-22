1fcd080fb5b1930ba255bb888f63602b1afccc27
Ben C <bwc9876@gmail.com>
2022-04-21 21:24:26 -0400

    Cool fixes

--- NHQSBCompat/QSBPatcher.cs
+++ NHQSBCompat/QSBPatcher.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using QSB.WorldSync;
 
 namespace NHQSBCompat
 {
@@ -15,11 +16,30 @@ namespace NHQSBCompat
             var joinLeaveSinularityCreate = typeof(JoinLeaveSingularity).GetMethod(nameof(JoinLeaveSingularity.Create));
             Main.Instance.ModHelper.HarmonyHelper.AddPrefix(joinLeaveSinularityCreate, typeof(QSBPatcher), nameof(QSBPatcher.OnJoinLeaveSingularityCreate));
             */
+            var WorldSyncOnBuildWorldObjects = typeof(QSBWorldSync).GetMethod(nameof(QSBWorldSync.BuildWorldObjects));
+            Main.Instance.ModHelper.HarmonyHelper.AddPrefix(WorldSyncOnBuildWorldObjects, typeof(QSBPatcher), nameof(OnQSBWorldSyncBuildWorldObjects));
         }
 
         public static bool OnJoinLeaveSingularityCreate()
         {
             return false;
+        }
+        
+        private static readonly string[] ManagersToRemove =
+        {
+            "OrbManager",
+            "SatelliteProjectorManager",
+            "MeteorManager",
+            "JellyfishManager",
+            "AnglerManager",
+            "OccasionalManager",
+        };
+        
+        public static void OnQSBWorldSyncBuildWorldObjects()
+        {
+            List<WorldObjectManager> managers = QSBWorldSync.Managers.ToList();
+            managers.RemoveAll(m => ManagersToRemove.Contains(m.ToString()));
+            QSBWorldSync.Managers = managers.ToArray();
         }
     }
 }
