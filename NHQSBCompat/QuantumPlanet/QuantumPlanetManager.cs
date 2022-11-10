852903c252794bb6c9c0f26373b3ee47eda62cb2
Mister_Nebula <41904486+misternebula@users.noreply.github.com>
2022-11-10 11:54:13 +0000

    add quick quantumplanet manager

---
+++ NHQSBCompat/QuantumPlanet/QuantumPlanetManager.cs
@@ -0,0 +1,17 @@
+﻿using Cysharp.Threading.Tasks;
+using NHQSBCompat.QuantumPlanet.WorldObjects;
+using QSB.WorldSync;
+using System.Threading;
+
+namespace NHQSBCompat.QuantumPlanet;
+
+internal class QuantumPlanetManager : WorldObjectManager
+{
+	public override WorldObjectScene WorldObjectScene => WorldObjectScene.Both;
+	public override bool DlcOnly => false;
+
+	public override async UniTask BuildWorldObjects(OWScene scene, CancellationToken ct)
+	{
+		QSBWorldSync.Init<QSBQuantumPlanet, NewHorizons.Components.Quantum.QuantumPlanet>();
+	}
+}
---
+++ NHQSBCompat/QuantumPlanet/WorldObjects/QSBQuantumPlanet.cs
@@ -0,0 +1,9 @@
+﻿using NewHorizons.Components.Quantum;
+using QSB.QuantumSync.WorldObjects;
+using QSB.WorldSync;
+
+namespace NHQSBCompat.QuantumPlanet.WorldObjects;
+
+public class QSBQuantumPlanet : QSBQuantumObject<NewHorizons.Components.Quantum.QuantumPlanet>
+{
+}
